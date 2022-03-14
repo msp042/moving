2331e4a67d332af48d38ae70dca920f8ad445504
anonymous anonymous <maya.patel@kellogg.northwestern.edu>
2022-03-14 18:34:44 +0000

    Whitelist columns in Item for ActiveAdmin

--- app/admin/items.rb
+++ app/admin/items.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Item do
 
+ permit_params :title, :box_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
