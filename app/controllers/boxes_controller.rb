d17bee62a4f8ddf469884ad41a9bf2d6abd7a304
anonymous anonymous <maya.patel@kellogg.northwestern.edu>
2022-03-14 18:35:51 +0000

    Remove controller comments

--- app/controllers/boxes_controller.rb
+++ app/controllers/boxes_controller.rb
@@ -4,27 +4,22 @@ class BoxesController < ApplicationController
 
   before_action :set_box, only: %i[show edit update destroy]
 
-  # GET /boxes
   def index
     @q = current_user.boxes.ransack(params[:q])
     @boxes = @q.result(distinct: true).includes(:user, :items,
                                                 :category).page(params[:page]).per(10)
   end
 
-  # GET /boxes/1
   def show
     @item = Item.new
   end
 
-  # GET /boxes/new
   def new
     @box = Box.new
   end
 
-  # GET /boxes/1/edit
   def edit; end
 
-  # POST /boxes
   def create
     @box = Box.new(box_params)
 
@@ -40,7 +35,6 @@ class BoxesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /boxes/1
   def update
     if @box.update(box_params)
       redirect_to @box, notice: "Box was successfully updated."
@@ -49,7 +43,6 @@ class BoxesController < ApplicationController
     end
   end
 
-  # DELETE /boxes/1
   def destroy
     @box.destroy
     message = "Box was successfully deleted."
@@ -70,12 +63,10 @@ class BoxesController < ApplicationController
     end
   end
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_box
     @box = Box.find(params[:id])
   end
 
-  # Only allow a trusted parameter "white list" through.
   def box_params
     params.require(:box).permit(:name, :user_id, :category_id)
   end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,25 +1,20 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: %i[show edit update destroy]
 
-  # GET /items
   def index
     @q = Item.ransack(params[:q])
     @items = @q.result(distinct: true).includes(:box, :category,
                                                 :user).page(params[:page]).per(10)
   end
 
-  # GET /items/1
   def show; end
 
-  # GET /items/new
   def new
     @item = Item.new
   end
 
-  # GET /items/1/edit
   def edit; end
 
-  # POST /items
   def create
     @item = Item.new(item_params)
 
@@ -35,7 +30,6 @@ class ItemsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /items/1
   def update
     if @item.update(item_params)
       redirect_to @item, notice: "Item was successfully updated."
@@ -44,7 +38,6 @@ class ItemsController < ApplicationController
     end
   end
 
-  # DELETE /items/1
   def destroy
     @item.destroy
     message = "Item was successfully deleted."
@@ -57,12 +50,10 @@ class ItemsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_item
     @item = Item.find(params[:id])
   end
 
-  # Only allow a trusted parameter "white list" through.
   def item_params
     params.require(:item).permit(:title, :box_id)
   end
