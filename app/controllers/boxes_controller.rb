9ff64af964a9e910d3d02336d38f8356e4123529
anonymous anonymous <maya.patel@kellogg.northwestern.edu>
2022-03-14 18:34:51 +0000

    Display associated collections on show pages

--- app/controllers/boxes_controller.rb
+++ app/controllers/boxes_controller.rb
@@ -42,8 +42,14 @@ class BoxesController < ApplicationController
   # DELETE /boxes/1
   def destroy
     @box.destroy
-    redirect_to boxes_url, notice: 'Box was successfully destroyed.'
+    message = "Box was successfully deleted."
+    if Rails.application.routes.recognize_path(request.referrer)[:controller] != Rails.application.routes.recognize_path(request.path)[:controller]
+      redirect_back fallback_location: request.referrer, notice: message
+    else
+      redirect_to boxes_url, notice: message
+    end
   end
+
 
   private
     # Use callbacks to share common setup or constraints between actions.
