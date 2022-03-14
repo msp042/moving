5c87730f092856479d47f1850fe91fef8fae85e9
anonymous anonymous <maya.patel@kellogg.northwestern.edu>
2022-03-14 18:35:11 +0000

    Generate graphiti resources for items

---
+++ app/resources/item_resource.rb
@@ -0,0 +1,12 @@
+class ItemResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :title, :string
+  attribute :box_id, :integer
+
+  # Direct associations
+
+  # Indirect associations
+
+end
