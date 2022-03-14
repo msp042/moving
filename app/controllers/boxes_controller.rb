193935757eabf48f49df6d4d28a5ef8298a42c62
anonymous anonymous <maya.patel@kellogg.northwestern.edu>
2022-03-14 18:34:57 +0000

    Only box user can modify box

--- app/controllers/boxes_controller.rb
+++ app/controllers/boxes_controller.rb
@@ -1,4 +1,6 @@
 class BoxesController < ApplicationController
+  before_action :current_user_must_be_box_user, only: [:edit, :update, :destroy] 
+
   before_action :set_box, only: [:show, :edit, :update, :destroy]
 
   # GET /boxes
@@ -58,6 +60,14 @@ class BoxesController < ApplicationController
 
 
   private
+
+  def current_user_must_be_box_user
+    set_box
+    unless current_user == @box.user
+      redirect_back fallback_location: root_path, alert: "You are not authorized for that."
+    end
+  end
+
     # Use callbacks to share common setup or constraints between actions.
     def set_box
       @box = Box.find(params[:id])
